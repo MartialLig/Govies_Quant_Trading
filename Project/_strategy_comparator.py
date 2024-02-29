779c93a074b397e604e101c179fe06d3fe4c6a5f
MarsLig <98904706+MartialLig@users.noreply.github.com>
2024-02-29 16:10:09 +0100

    maj correlation

--- Project/_strategy_comparator.py
+++ Project/_strategy_comparator.py
@@ -8,6 +8,14 @@ class StratgiesComparetor():
         pass
 
     def compare_results_of_strategies(self):
+        """
+        Aggregates results from each strategy, renames the columns to match the strategy names,
+        and concatenates them into a single DataFrame for comparison.
+
+        Returns:
+        - pd.DataFrame: A DataFrame where each column represents the aggregated results of a strategy,
+          with column names corresponding to `list_name_strategy`.
+        """
         list_of_agregate_perf_data = []
         for counter, strategy in enumerate(self.list_straties):
             new_data = strategy.aggregate_results()
@@ -16,12 +24,22 @@ class StratgiesComparetor():
         return pd.concat(list_of_agregate_perf_data, axis=1)
 
     def plot_everything(self):
+        """
+        Plots the P&L for each strategy using the strategy's `plot_p_and_l` method.
+        """
         for compteur, strategy in enumerate(self.list_straties):
             strategy.plot_p_and_l(self.list_name_strategy[compteur])
 
         return
 
     def combine_p_and_l_datasets(self):
+        """
+        Combines the P&L datasets from all strategies into a single DataFrame.
+
+        Returns:
+        - pd.DataFrame: A DataFrame where each column represents the P&L data from a strategy,
+          with column names corresponding to `list_name_strategy`
+        """
         combined_p_and_l = pd.DataFrame()
         for counter, strategy in enumerate(self.list_straties):
             if strategy.p_and_l_dataset is not None:
@@ -30,5 +48,11 @@ class StratgiesComparetor():
         return combined_p_and_l
 
     def calculate_correlations(self):
+        """
+        Calculates and returns the correlation matrix of the P&L datasets across all strategies.
+
+        Returns:
+        - pd.DataFrame: A correlation matrix of the P&L datasets from each strategy.
+        """
         combined_p_and_l = self.combine_p_and_l_datasets()
         return combined_p_and_l.corr()
